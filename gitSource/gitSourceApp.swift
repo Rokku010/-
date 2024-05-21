c1a3e2b6fd7adc7868ffe4a6f223a9be3c2ad16c
Rokku <quocloctran357@gmail.com>
2024-05-21 13:57:16 +0700

    Initial Commit

---
+++ gitSource/gitSourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitSourceApp.swift
+//  gitSource
+//
+//  Created by Quốc Lộc on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct gitSourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
